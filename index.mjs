248e5eaeb507712e4ff46645ad120f151e923917
OhHiMerq <dimailigv@gmail.com>
2023-05-03 23:31:29 +0800

    init

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import CubejsServer from "@cubejs-backend/server";
+
+const server = new CubejsServer();
+
+server
+  .listen()
+  .then(({ version, port }) => {
+    console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
+  })
+  .catch((e) => {
+    console.error("Fatal error during server start: ");
+    console.error(e.stack || e);
+  });
